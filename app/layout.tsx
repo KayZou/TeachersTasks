3ef5de66e36e6dc598b12f90663fccee7d53e0fe
kayzou <elghazzalyzakaria@gmail.com>
2025-05-08 14:12:12 +0100

    metadata & ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Teachers Tasks",
+  description: "Kanban Wep App Board For Schools"
+}
 
 export default function RootLayout({
   children,
